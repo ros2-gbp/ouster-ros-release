75385e637c258adc305b60bb4bf463b82ee3dfbd
Ussama Naal <ussama.naal@ouster.io>
2026-03-09 15:31:09 -0700

    Trimmed the branch to only the ouster-sensor-msgs sub directory

--- ouster-ros/ouster-sdk/cmake/Findspdlog.cmake
+++
@@ -1,6 +0,0 @@
-# Allow downstream code to depend on source transparently
-if(NOT SPDLOG_INCLUDED)
-  add_custom_target(SPDLOG_INCLUDED)
-  add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../thirdparty/spdlog
-    ${CMAKE_CURRENT_BINARY_DIR}/spdlog EXCLUDE_FROM_ALL)
-endif()
